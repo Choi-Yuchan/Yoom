16421bc84771edf608d38f70441c2e57301b7b46
Choi-Yuchan <cor330@gmail.com>
2022-01-11 18:28:11 +0900

    chat with websockets

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -5,9 +7,32 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-app.get("/", (req, res) => res.render("home"));
+const handleListen = () => console.log(`Listening on http://localhost:3030`);
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-app.listen(3000, handleListen);
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Default";
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
+  });
+});
+
+server.listen(3030, handleListen);
